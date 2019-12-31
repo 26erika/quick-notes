046182a2517aa6c8b4ba223472f528c243a08609
26erika <develop.erika@gmail.com>
2019-12-31 10:55:09 -0600

    Modified and create elements to principal components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './Note/list/list.component';
+import { AddComponent } from './Note/add/add.component';
+import { EditComponent } from './Note/edit/edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'list', component:ListComponent},
+  {path:'add', component:AddComponent},
+  {path:'edit', component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
